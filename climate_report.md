2a439eea152b760da8a11461237d7895977e5ccd
Ott Toomet <otoomet@uw.edu>
2017-11-01 16:49:58 -0700

    remote mapping exercise instead of shooting

---
+++ climate_report.md
@@ -0,0 +1,25 @@
+# Climate Report
+
+## North American Temperature and Precipitation, 1960-2014
+
+### Temperature
+
+insert your temperature images here
+
+comment the results.  Do the temperature patterns look reasonable?  Do
+you see major differences between years?
+
+
+### Precipitation
+
+insert your precipitation images here
+
+comment the results.  Do the precipitation patterns look reasonable?  Do
+you see major differences between years?
+
+
+### Note: 
+
+you cannot do any conclusions about long-term trend based on just 3
+figures.  If you are interested, you are welcome to explore long-term
+trends in these data, but be sure to use more suitable methods.
---
+++ maps.R
@@ -0,0 +1,22 @@
+### This is the stump script to read the data and plot the maps
+
+## read the data
+
+## filter out North American observations
+
+## delete the original (large data) from R workspace
+
+## -------------------- do the following for 1960, 1987, 2014 and temp/precipitation --------------------
+
+## select jpg graphics device
+
+## select the correct year - plot longitude-latitude and color according to the temp/prec variable
+## I recommend to use ggplot() but you can do something else
+## Note: if using ggplot, you may want to add "+ coord_map()" at the end of the plot.  This
+## makes the map scale to look better.  You can also pick a particular map projection, look
+## the documentation.  (You need 'mapproj' library).
+
+## close the device
+
+
+
