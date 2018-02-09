011ee9ae2bfd1b6755360bf9ed7c1fe1df05b605
Ott Toomet <otoomet@uw.edu>
2018-02-09 15:20:23 -0800

    some more text in session.sh

--- session.sh
+++ session.sh
@@ -3,9 +3,12 @@
 # document here the main bash commands you are using for the server access.  The file contains
 # two parts: the first one for the commands you run on your laptop, and the second one for the
 # commands on the remote server.
+# 
 # The question lists give your some guidance what to do and report but you are free to do it
-# in a different order.  If you use a graphical frontend instead of scp for copying files,
-# just say that in the appropriate place.
+# in a different way.  If you use a graphical frontend instead of scp for copying files,
+# just say which one in the appropriate place.
+#
+# Note: this file does not to be runnable
 #
 # -------------------- local commands --------------------
 # 1. create a directory for this project.
@@ -22,7 +25,7 @@
 
 
 # -------------------- remote commands --------------------
-# 1. explore the data directory '/opt/data'
+# 1. explore the data directory '/opt/data'.  How do you find out the size of the files?
 
 # 2. explore the first few lines of the small sample data
 
