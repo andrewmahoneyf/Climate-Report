65f244cbf4170eb25a2b3c755853cf664b50d76a
andrewmahoneyf <andrew.mahoney.f@gmail.com>
2018-02-16 23:05:35 -0800

    Initial steps and outline of project complete

--- maps.R
+++ maps.R
@@ -6,7 +6,10 @@
 ### The file must be executable on the server!
 ### I.e. someone else must be able to just run it with 'Rscript maps.R'
 ### /on server/ and get the correct output.
-
+install.packages("data.table")
+install.packages("dplyr")
+library("data.table")
+library(dplyr)
 
 ## read the data
 ##
@@ -15,18 +18,32 @@
 ## hint2: read.table and friends are slow (about 6 min to read data).
 ## You may use data.table::fread instead (a few seconds)
 
+require(data.table)
+system.time(DT <- fread(sprintf("bzcat %s | tr -d '\\000'", "./data/temp_prec_subset.csv.bz2")))
+##   user  system  elapsed 
+##  0.045   0.010   0.049 
+system.time(DT <- fread(sprintf("bzcat %s | tr -d '\\000'", "./data/temp_prec_small.csv.bz2")))
+##   user  system  elapsed 
+##  92.005  3.469  77.314  
 
 ## filter out North American observations
-
+## Latitude between 24 and 65 degrees N 
+## Longitude Western hemisphere > 180 degrees E
+NorthAmerica <- DT %>% filter(longitude > 180, latitude > 24, latitude < 65)
 
 ## delete the original (large data) from R workspace
 ## this is necessary to conserve memory.
-
+rm(DT)
 
 ## -------------------- do the following for 1960, 1987, 2014 and temp/precipitation --------------------
 
 ## select jpg graphics device
-
+jpeg(filename = "Rplot%03d.jpeg",
+     width = 480, height = 480, units = "px", pointsize = 12,
+     quality = 75,
+     bg = "white", res = NA, ...,
+     type = c("cairo", "Xlib", "quartz"), antialias)
+     
 ## select the correct year - plot longitude-latitude and color according to the temp/prec variable
 ## I recommend to use ggplot() but you can use something else too.
 ## 
--- session.sh
+++ session.sh
@@ -12,29 +12,38 @@
 #
 # -------------------- local commands --------------------
 # 1. create a directory for this project.
-
+mkdir data
 # 2. log onto the server
-
+ssh drew102@info201.ischool.uw.edu
 # 3. copy the small data subset from the server to your local machine
-
+exit
+scp drew102@info201.ischool.uw.edu:/opt/data/temp_prec_subset.csv.bz2 ./data/
 # 4. copy your R-script to the server
-
+sudo chmod +x maps.R
+scp ./maps.R drew102@info201.ischool.uw.edu:~/drew102-a6
 # 5. (after succesfully running the script remotely): copy the output files back to your computer
-
+scp otoomet@info201.ischool.uw.edu:~/drew102-a6/*.jpeg .
 # 6. inspect that the copy was successful
-
+ls
+open 
 
 # -------------------- remote commands --------------------
 # 1. explore the data directory '/opt/data'.  How do you find out the size of the files?
-
+ssh drew102@info201.ischool.uw.edu
+cd /opt/data
+ls -l 
 # 2. explore the first few lines of the small sample data
-
+bzcat temp_prec_small.csv.bz2 | head -10
 # 3. create a directory for this project
-
+cd ~ && mkdir drew102-a6
 # 4. (after copying your code from the laptop): inspect the files in the project directory
-
+cd drew102-a6 && ls -al
 # 5. run your script
-
+Rscript maps.R
 # 6. How do you install missing R packages?
-
+R
+install.packages("foo")
+# or
+Rscript -e "install.packages('foo')"
 # 7. (after running the script): inspect the folder for output files
+ls
